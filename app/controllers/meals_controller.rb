d818f27efe4c03e59937da1f47f2aeece19b7f94
John Hinrichs <jmhinric@gmail.com>
2016-09-24 17:41:54 -0500

    Adds meals

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,34 @@
+class MealsController < ApplicationController
+  def create
+    meal = Meal.create!(description: description, start_time: start_time, end_time: end_time, program: program)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def program
+    Program.find(params["program_id"])
+  end
+
+  def description
+    params["meal"]["description"]
+  end
+
+  def start_time
+    datetime("start_time")
+  end
+
+  def end_time
+    datetime("end_time")
+  end
+
+  def datetime(f)
+    DateTime.new(year(f), month(f), day(f), hour(f), minute(f))
+  end
+
+  %i(year month day hour minute).each_with_index do |method_name, i|
+    define_method(method_name) do |field|
+      params["meal"]["#{field}(#{i+1}i)"].to_i
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:show, :update]
+  resources :programs do
+    resources :meals, only: [:create]
+  end
 end
